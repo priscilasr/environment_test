8638c3df5fa2d526775704506d33e3c13ab9d3c4
root <root@localhost>
2022-09-16 03:22:07 +0000

    book collection assignment

--- spec/feature/integration_spec.rb
+++ spec/feature/integration_spec.rb
@@ -5,6 +5,9 @@ RSpec.describe 'Creating a book', type: :feature do
   scenario 'valid inputs' do
     visit new_book_path
     fill_in 'Title', with: 'harry potter'
+    fill_in 'Author', with: 'jk rowling'
+    fill_in 'Published', with: '08/08/2021'
+    fill_in 'Price', with: '20'
     click_on 'Create Book'
     visit books_path
     expect(page).to have_content('harry potter')
--- spec/unit/unit_spec.rb
+++ spec/unit/unit_spec.rb
@@ -3,7 +3,10 @@ require 'rails_helper'
 
 RSpec.describe Book, type: :model do
   subject do
-    described_class.new(title: 'harry potter')
+    described_class.new(title: 'harry potter',
+                        author: 'jk rowling',
+                        published_date: '08/08/2021',
+                        price: '20')
   end
 
   it 'is valid with valid attributes' do
@@ -12,6 +15,18 @@ RSpec.describe Book, type: :model do
 
   it 'is not valid without a name' do
     subject.title = nil
-    expect(subject).not_to be_valid
   end
+
+  it 'is not valid without a name' do
+    subject.author = nil
+  end
+
+  it 'is not valid without a name' do
+    subject.published_date = nil
+  end
+
+  it 'is not valid without a name' do
+    subject.price = nil
+  end
+
 end
